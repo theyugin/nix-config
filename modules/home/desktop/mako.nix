0feaa05b336db522d5622f7c1f33ed542e54b44a
theyugin <theyugin@gmail.com>
2025-12-09 05:49:37 +0500

    mako

--- modules/home/desktop/mako.nix
+++ modules/home/desktop/mako.nix
@@ -1,4 +1,10 @@
 {
-  services.mako.enable = true;
+  services.mako = {
+    enable = true;
+    settings = {
+      default-timeout = 5000;
+      anchor = "bottom-right";
+    };
+  };
   stylix.targets.mako.enable = true;
 }
